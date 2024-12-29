71e2e95804989cfd6f86eeec577f50f8be34dc64
vs24-c <slvscoach2018@gmail.com>
2024-12-29 22:48:29 +0200

    Update RequestManager.js

--- script/components/RequestManager.js
+++ script/components/RequestManager.js
@@ -1,6 +1,7 @@
 class RequestManager {
   // Базовий URL для API запитів
-  static apiBase = 'http://localhost:3000/api/v1';
+  // static apiBase = 'http://localhost:3000/api/v1';
+  static apiBase = 'https://task-12-backend.onrender.com';
   // static apiBase = '';
 
   // Метод для отримання повного маршруту до сервера
